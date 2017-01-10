c055f21c80c627b321603f5d64a4d2e9c112768e
Jeong H Park <hello@jeonghyuk.info>
2017-01-09 19:18:11 -0500

    Creating Entities
    Completed p. 176 of 476

---
+++ src/OpenGameListWebApp/Data/Comments/Comment.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OpenGameListWebApp.Data.Comments
+{
+    public class Comment
+    {
+        public Comment() { }
+
+        [Key]
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public int ItemId { get; set; }
+
+        [Required]
+        public string Text { get; set; }
+
+        [Required]
+        public int Type { get; set; }
+
+        [Required]
+        public int Flags { get; set; }
+
+        [Required]
+        public string UserId { get; set; }
+
+        [Required]
+        public int? ParentId { get; set; }
+
+        [Required]
+        public DateTime CreatedDate { get; set; }
+
+        [Required]
+        public DateTime LastModifiedDate { get; set; }
+    }
+}
