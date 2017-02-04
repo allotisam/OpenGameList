a8447d91d90fdac59130a121f7c5989e252e3aba
Jeong H Park <hello@jeonghyuk.info>
2017-02-04 16:45:35 -0500

    Apply Styles
    Completed p.298 of 476

--- src/OpenGameListWebApp/gulpfile.js
+++ src/OpenGameListWebApp/gulpfile.js
@@ -1,4 +1,4 @@
-/// <binding />
+/// <binding Clean='default' />
 /*
 This file in the main entry point for defining Gulp tasks and using Gulp plugins.
 Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
@@ -21,8 +21,8 @@ var srcPaths = {
         'node_modules/reflect-metadata/Reflect.js',
         'node_modules/systemjs/dist/system.src.js',
         'node_modules/typescript/lib/typescript.js',
-        'node_modules/ng2-bootstrap/bundles/ng2-bootstrap.umd.min.js',
-        'node_modules/moment/moment.js'
+        'node_modules/moment/moment.js',
+        'node_modules/ng2-bootstrap/bundles/ng2-bootstrap.umd.min.js'
     ],
     js_angular: [
         'node_modules/@angular/**'
