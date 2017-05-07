7fbfc8a7da8c0178f3e4cb5ed035aaacdbb62837
Jeong H Park <hello@jeonghyuk.info>
2017-05-07 15:36:48 -0400

    Creating Entities completed p.177 of 476

---
+++ src/OpenGameListWebApp/Data/Items/Item.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace OpenGameListWebApp.Data.Items
+{
+    public class Item
+    {
+        #region Constructor
+
+        public Item() { }
+
+        #endregion Constructor
+        
+        #region Properties
+
+        [Key, Required]
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public string Text { get; set; }
+
+        public string Notes { get; set; }
+
+        [Required]
+        public int Type { get; set; }
+
+        [Required]
+        public int Flags { get; set; }
+
+        [Required]
+        public string UserId { get; set; }
+
+        [Required]
+        public int ViewCount { get; set; }
+
+        [Required]
+        public DateTime CreatedDate { get; set; }
+
+        [Required]
+        public DateTime LastModifiedDate { get; set; }
+
+        #endregion Properties
+
+    }
+}
