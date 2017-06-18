b70775805c259c857c991b3180f2ba96815b8d50
Jeong H Park <hello@jeonghyuk.info>
2017-06-17 21:59:29 -0400

    Setting Up Client-Code completed p.73 of 476

---
+++ src/OpenGameListWebApp/Scripts/app/app.component.ts
@@ -0,0 +1,7 @@
+﻿import { Component } from "@angular/core";
+
+@Component({
+    selector: "opengamelist",
+    template: `<h1>Opengamelist</h1><div>Work in progress...the best is yet to come!</div>`
+})
+export class AppComponent { }
