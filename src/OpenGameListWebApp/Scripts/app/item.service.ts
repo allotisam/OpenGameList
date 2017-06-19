87ff44a92df61061964fa31086ea48a862c4fa6b
Jeong H Park <hello@jeonghyuk.info>
2017-06-19 18:45:26 -0400

    Master-Detail Binding completed p.127 of 476

---
+++ src/OpenGameListWebApp/Scripts/app/item.service.ts
@@ -0,0 +1,50 @@
+﻿import { Injectable } from "@angular/core";
+import { Http, Response } from "@angular/http";
+import { Observable } from "rxjs/Observable";
+import { Item } from "./item";
+
+@Injectable()
+export class ItemService {
+
+    private baseUrl = "api/items/";
+
+    constructor(private http: Http) { }
+
+    getLatest(num?: number) {
+        var url = this.baseUrl + "GetLatest/";
+        if (num != null) {
+            url += num;
+        }
+
+        return this.http.get(url)
+            .map(response => response.json())
+            .catch(this.handleError);
+    }
+
+    getMostViewed(num?: number) {
+        var url = this.baseUrl + "GetMostViewed/";
+        if (num != null) {
+            url += num;
+        }
+
+        return this.http.get(url)
+            .map(response => response.json())
+            .catch(this.handleError);
+    }
+
+    getRandom(num?: number) {
+        var url = this.baseUrl + "GetRandom/";
+        if (num != null) {
+            url += num;
+        }
+
+        return this.http.get(url)
+            .map(response => response.json())
+            .catch(this.handleError);
+    }
+
+    private handleError(error: Response) {
+        console.error(error);
+        return Observable.throw(error.json().error || "Server Error");
+    }
+}
