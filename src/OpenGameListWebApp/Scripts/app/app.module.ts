6f45711b28bedd5c3b47301432eafddc26a64cab
Jeong H Park <hello@jeonghyuk.info>
2017-08-19 17:49:51 -0400

    JSON Web Token -Angular 2 Login Form completed p.339 of 476

--- src/OpenGameListWebApp/Scripts/app/app.module.ts
+++ src/OpenGameListWebApp/Scripts/app/app.module.ts
@@ -2,7 +2,7 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { HttpModule } from "@angular/http";
-import { FormsModule } from "@angular/forms";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { RouterModule } from "@angular/router";
 import "rxjs/Rx";
 
@@ -23,6 +23,7 @@ import { ItemService } from "./item.service";
         BrowserModule,
         HttpModule,
         FormsModule,
+        ReactiveFormsModule,
         RouterModule,
         AppRouting
     ],
